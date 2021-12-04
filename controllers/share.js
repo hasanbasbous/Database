59437763535a3a322704b1b3d7097e46bddd8e9a
hasanbasbous <hasan.basbous@lau.edu>
2021-12-04 17:16:06 +0200

    shareWorking

--- controllers/share.js
+++ controllers/share.js
@@ -12,14 +12,86 @@ const db = mysql.createConnection({
 exports.share = (req, res) => {
     console.log(req.body);
     const {from, to, date, time, length, spots} = req.body;
+    var  tripId;
+    var  s, d;
     
-    db.query("INSERT INTO trips SET ?", {source: from, destination: to, date: date, time: time, length: length, nbSeats: spots, driverId: req.session.userId,
-    role: 'driver'}, (error, results) => {
+    db.query("INSERT INTO trip SET ?", {source: from, destination: to, date: date, time: time, length: length, nbSeats: spots, driverId: req.session.userId}, (error, results) => {
             if(error){
                 console.log(error);
             } else {
-                console.log(results);
-                return res.redirect('/trips');
-            }
+                tripId = results.insertId;
+                console.log(tripId);
+
+                for(let x = spots; x > 0; x--){
+                    db.query ( "INSERT INTO seat SET ?", {status: 'available' ,rideID: tripId},  
+                        (error, results) => {
+                            if(error){
+                                console.log(error);
+                            } else {
+                                console.log(results);
+                            };
+                        })
+                    }
+                
+                db.query('SELECT stopID FROM stop WHERE address =  "'+ from + '"' ,(error, results) =>  {
+                    if(error){
+                        console.log(error);
+                    } else {
+                        var q =JSON.parse(JSON.stringify(results))
+                        console.log(q)
+                        s = q[0].stopID;
+                        // s = parseInt(q[0].stopID, 10)
+                        console.log(s)
+                    }			    
+                });
+                db.query('SELECT stopID FROM stop WHERE address = "' + to + '"' ,(error, results) =>  {
+                    if(error){
+                        console.log(error);
+                    } else {
+                        var q =JSON.parse(JSON.stringify(results))
+                        console.log(q)
+                        d = q[0].stopID;
+                        // d = parseInt(q[0].stopID, 10)
+                        console.log(typeof(d))
+
+                        // if(d > s)
+                        //     console.log("entered condition")
+                //         if(d > s)
+                //             console.log("entered condition")
+                // console.log('entered')
+
+                        if (s > d){
+                            while(s >= d){
+                                console.log('entered')
+                                db.query ( "INSERT INTO passingthrough SET ?", {stopID: s ,rideID: tripId},  
+                                (error, results) => {
+                                    if(error){
+                                        console.log(error);
+                                    } else {
+                                        console.log(results);
+                                    };
+                                })
+                                s--;
+                            }
+                        }    
+                        else if(s<d){   
+                            while(s<=d){
+                                console.log('entered')
+                                db.query ( "INSERT INTO passingthrough SET ?", {stopID: s ,rideID: tripId},  
+                                (error, results) => {
+                                    if(error){
+                                        console.log(error);
+                                    } else {
+                                        console.log(results);
+                                    };
+                                })
+                                s++;
+                        }
+                        }
+                    }			    
+                });
+                
+                res.redirect('/trips')
+             }
         })
     }
